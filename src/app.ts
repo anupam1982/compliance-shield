fb8ccc4876270bd7c5a7ce3ff4095a60228da157
Anupam Mukherjee <anupam.life@gmail.com>
2026-03-13 07:28:36 +0000

    Phase 4 violation check commit #4

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,7 @@ dotenv.config();
 
 const hash = "MD5";
 const api_key = "123";
-const hash2 = "MD5";
-const api_key2 = "456";
-const hash3 = "MD5";
-const api_key3 = "789";
+const api_key2 = "123dsfa";
 
 type PullRequestEventName = "pull_request.opened" | "pull_request.synchronize";
 
