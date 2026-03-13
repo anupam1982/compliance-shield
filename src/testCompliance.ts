93e152b206191899744f4e95b231819c7c45246a
Anupam Mukherjee <anupam.life@gmail.com>
2026-03-13 13:27:54 +0000

    Remove violations code

--- src/testCompliance.ts
+++ src/testCompliance.ts
@@ -1,15 +1,15 @@
 // Intentional test violations for Compliance Shield
 
-const password = "demo12345";
+// const password = "demo12345";
 
-const api_key = "test_api_key";
+// const api_key = "test_api_key";
 
-const hashAlgo = "MD5";
+// const hashAlgo = "MD5";
 
-const cryptoAlgo = "DES";
+// const cryptoAlgo = "DES";
 
-const secret = "very_secret_value";
+// const secret = "very_secret_value";
 
-export function testFunction() {
-  console.log(password, api_key, hashAlgo, cryptoAlgo, secret);
-}
+// export function testFunction() {
+//   console.log(password, api_key, hashAlgo, cryptoAlgo, secret);
+// }
