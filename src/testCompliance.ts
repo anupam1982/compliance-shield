d2fc878d9464538c7e704909284afcbf851194aa
Anupam Mukherjee <anupam.life@gmail.com>
2026-03-14 04:53:51 +0000

    Remove test violations

--- src/testCompliance.ts
+++ src/testCompliance.ts
@@ -1,6 +1,1 @@
 // Intentional test violations for Compliance Shield
-const token1 = "TEST_TOKEN_ABC123";
-const token2 = "TEST_TOKEN_ABC123";
-const hash1 = "MD5";
-const hash2 = "MD5";
-const hash3 = "MD5";
