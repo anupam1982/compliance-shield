568a140175cca2b9741683d0ea725434625d4dcf
Anupam Mukherjee <anupam.life@gmail.com>
2026-05-21 15:48:04 +0800

    feat:Add Missing tableand migrations

--- src/db/migrations/runMigrations.ts
+++ src/db/migrations/runMigrations.ts
@@ -1,11 +1,13 @@
 import { createOverrideAuditsTable } from "./createOverrideAuditsTable";
 import { updateOverrideAuditsTable } from "./updateOverrideAuditsTable";
 import { updateScanMetricsTable } from "./updateScamMetricsTable";
+import { runUsageEventsMigration } from "./createUsageEventsTable";
 
 export async function runMigrations() {
   console.log("Running database migrations...");
   await createOverrideAuditsTable();
   await updateOverrideAuditsTable();
   await updateScanMetricsTable();
+  await runUsageEventsMigration();
   console.log("Database migrations completed.");
 }
